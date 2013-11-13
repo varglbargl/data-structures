ccc409bc8e38ce3aa233cfaeeb513f2c31205f54
Eric <ehannum@gmail.com>
2013-11-13 12:51:43 -0800

    innitial commit. no specs passing.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,5 +1,38 @@
 var makeQueue = function(){
-  // Hey! Copy your code from src/functional/queue.js and paste it here
+  var instance = {};
+
+  // Use an object with numeric keys to store values
+  var storage = {};
+  var counter = 0;
+
+  // Implement the methods below
+
+  instance.enqueue = function(value){
+    storage[counter] = value;
+    counter++;
+    return storage;
+  };
+
+  instance.dequeue = function(){
+    var first = storage[0];
+    for (var key in storage){
+      storage[key - 1] = storage[key];
+    }
+    delete storage[counter];
+    counter--;
+    return first;
+  };
+
+  instance.size = function(){
+    if(counter < 0){
+      counter = 0;
+      return counter;
+    } else {
+      return counter;
+    }
+  };
+
+  return instance;
 };
 
 var queueMethods = {};

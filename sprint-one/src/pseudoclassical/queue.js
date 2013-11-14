dff8f999fb3d1b6f7b99e9251706a9ca2b8a2ef3
Eric <ehannum@gmail.com>
2013-11-13 16:28:45 -0800

    queue pseudoclassical passing all specs

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,3 +1,35 @@
 var Queue = function() {
   // Hey! Copy your code from src/prototypal/queue.js and paste it here
+  // Use an object with numeric keys to store values
+  this.storage = {};
+  this.length = 0;
+  // Implement the methods below
+};
+
+Queue.prototype = {
+  constructor: Queue,
+  enqueue: function(value){
+    this.storage[this.length] = value;
+    this.length++;
+    return this.storage;
+  },
+
+  dequeue: function (){
+    var first = this.storage[0];
+    for (var key in this.storage){
+      this.storage[key - 1] = this.storage[key];
+    }
+    delete this.storage[this.length];
+    this.length--;
+    return first;
+  },
+
+  size: function (){
+    if(this.length < 0){
+      this.length = 0;
+      return this.length;
+    } else {
+      return this.length;
+    }
+  }
 };

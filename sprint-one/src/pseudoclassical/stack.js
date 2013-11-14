65be3d2250c02257feb0439bd744f66635c1d3b3
Eric <ehannum@gmail.com>
2013-11-13 16:25:56 -0800

    pseudoclassical stack passing all specs

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,3 +1,27 @@
 var Stack = function() {
-  // Hey! Copy your code from src/prototypal/stack.js and paste it here
+  // Hey! Copy your code from src/functional/stack.js and paste it here
+  // Use an object with numeric keys to store values
+  this.storage = {};
+  this.length = 0; // Hint: set an initial value here
+
+  // Implement the methods below
+};
+
+Stack.prototype = {
+  constructor: Stack,
+  push: function(value){
+    this.storage[this.length] = value;
+    this.length++;
+  },
+  pop: function(){
+    var last = this.storage[this.length-1];
+    delete this.storage[this.length];
+    if(this.length !== 0){
+      this.length--;
+    }
+    return last;
+  },
+  size: function(){
+    return this.length;
+  }
 };

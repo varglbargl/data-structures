ad5c9e4ecd09060f557c58e231628a1555611339
Eric <ehannum@gmail.com>
2013-11-14 19:27:38 -0800

    passing initial add,contains,and remove methods in set

--- sprint-two/spec/setSpec.js
+++ sprint-two/spec/setSpec.js
@@ -10,4 +10,17 @@ describe("set", function() {
     expect(set.contains).toEqual(jasmine.any(Function));
     expect(set.remove).toEqual(jasmine.any(Function));
   });
+  it("should add strings to the set", function() {
+    set.add("hello");
+    expect(set._storage[0]).toEqual("hello");
+  });
+  it("should find strings", function() {
+    set.add("wakka wakka");
+    expect(set.contains("wakka wakka")).toEqual(true);
+  });
+  it("should delete strings", function(){
+    set.add("testing is boring");
+    set.remove("testing is boring");
+    expect(set.contains("testing is boring")).toEqual(false);
+  });
 });
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,16 +1,20 @@
 var makeSet = function(){
   var set = Object.create(setPrototype); // fix me
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
-setPrototype.add = function(){
+setPrototype.add = function(val){
+  this._storage.push(val);
 };
 
-setPrototype.contains = function(){
+setPrototype.contains = function(val){
+  return (this._storage.indexOf(val) !== -1);
 };
 
-setPrototype.remove = function(){
+setPrototype.remove = function(val){
+  this._storage.splice(this._storage.indexOf(val),1);
 };
+  

4279cb8bee301665b295855c9aecbc868bf46119
ehannum <ehannum@gmail.com>
2013-12-03 22:51:26 -0800

    late night refactoring of prefix tree to use the 10,000 most common english words instead of attempting the 161,000 of the scrabble dictionary.

--- sprint-two/src/app.js
+++ sprint-two/src/app.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
   var prefixTree = new PrefixTree();
-  prefixTree.build(longerWordlist);
+  prefixTree.build(longWordlist);
 
   $(".word").keyup(function () {
     var input = $(".word").val();
-    console.log(input);
-    if(input.length > 2){
+    // console.log(input);
+    if(input.length > 0){
       var words = prefixTree.lookup( input );
       var list = words.join("</h3><h3>")
       $(".output").html("<h3>" + list + "</h3>");
